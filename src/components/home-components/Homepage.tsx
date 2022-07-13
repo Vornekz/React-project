dd1c74f34c18accdce3cb82f92474e623c8c9720
Vornekz <6319553@gmai.com>
2022-07-13 12:08:05 +0300

    Start react project

---
+++ src/components/home-components/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
+const Homepage = () => {
+    return (
+        <>
+            <div className="home-page">
+
+            </div>
+
+        </>
+    );
+};
+
+export default Homepage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Homepage from "./components/home-components/Homepage";
+import Homepage2 from "./components/home-components/Homepage2";
+import Homepage3 from "./components/home-components/Homepage3";
+import Home from "./components/Home";
+import Pages from "./components/Pages";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route path="/home" element={<Home/>}>
+                        <Route path="/homepage" element={<Homepage/>}/>
+                        <Route path="/homepage2" element={<Homepage2/>}/>
+                        <Route path="/homepage3" element={<Homepage3/>}/>
+                    </Route>
+                    <Route path="/pages" element={<Pages/>}>
+
+                    </Route>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
